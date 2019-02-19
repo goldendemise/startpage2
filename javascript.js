5bc8d90b7dc136766f99a649b14c313bdf2c5d8d
drudgesentinel <drudgesentinel@protonmail.com>
2019-02-18 18:38:12 -0800

    Fixed js

--- javascript.js
+++ javascript.js
@@ -1,13 +1,6 @@
-const TODAY = new Date();
-let dayOfWeek = TODAY.getDay();
-let monthOfYear = TODAY.getMonth();
-let dayOfMonth = TODAY.getDate();
-let hour = TODAY.getHours();
-let minute = TODAY.getMinutes();
 
-//both day of week and month are zero indexed digits, creating arrays to properly display in human readable form
-let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+
 //modify DOM
 let dateDiv = document.querySelector('.date');
 let hourSpan = document.querySelector('#hour');
@@ -15,11 +8,24 @@ let minuteSpan = document.querySelector('#minute');
 
 //encapsulate updates in a function to run in settimeout
 function updateMarkup() {
+  const TODAY = new Date();
+  let dayOfWeek = TODAY.getDay();
+  let monthOfYear = TODAY.getMonth();
+  let dayOfMonth = TODAY.getDate();
+  let hour = TODAY.getHours();
+  let minute = TODAY.getMinutes();
+
+  //both day of week and month are zero indexed digits, creating arrays to properly display in human readable form
+  let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
 dateDiv.innerHTML = weekday[dayOfWeek] + ", " + months[monthOfYear] + " " + dayOfMonth;
 hourSpan.innerHTML = hour;
 minuteSpan.innerHTML = ":" + minute;
+cl("RAN");
 }
-var updateClock = setInterval(updateMarkup, 2000);
+updateMarkup();
+let updateClock = setInterval(updateMarkup, 5000);
 
 function cl(args) {
   console.log(args);
